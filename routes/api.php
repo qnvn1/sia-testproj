6a0c24424efc8cfac3169281e8f665c7d2151d32
Dan Ivan E. Labin <danivanlabin@gmail.com>
2025-06-01 21:21:26 +0800

    aw

--- routes/api.php
+++ routes/api.php
@@ -133,7 +133,11 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/meal-plan/items', [MealPlanController::class, 'saveMealItem']);
     });
 });
-    Route::get('/test', function () {
-    return response()->json(['status' => 'ok']);
+    Route::get('/test-advice/{id}', function ($id, AdviceSlipService $service) {
+    $advice = $service->getAdviceById($id);
+    if (!$advice) {
+        return response()->json(['error' => 'No advice found'], 404);
+    }
+    return response()->json($advice);
 });
 
